7de723392df30e2a174ea11b9ccc4247166880f7
zadnap <88957683+zadnap@users.noreply.github.com>
2026-04-25 17:40:47 +0700

    chore: add API boilerplate and register JWT

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+
+load_dotenv()
 
 def create_app():
-    app = Flask(__name__)
-
     from .routes import bp
+
+    app = Flask(__name__)
     app.register_blueprint(bp)
+    app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
+    jwt = JWTManager(app)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,56 @@ from flask import Blueprint, jsonify
 
 bp = Blueprint("main", __name__)
 
-@bp.route("/")
-def home():
-    return jsonify({"message": "API running"})
+@bp.route("/auth/sign-in", methods=["POST"])
+def sign_in():
+    return jsonify({"message": "Sign in success"})
+
+
+@bp.route("/auth/sign-up", methods=["POST"])
+def sign_up():
+    return jsonify({"message": "Sign up success"})
+
+
+@bp.route("/onboard", methods=["POST"])
+def onboard():
+    return jsonify({"message": "Onboarding success"})
+
+
+@bp.route("/favourites", methods=["GET"])
+def get_user_favourites():
+    return jsonify({"message": "Getting favourite movies success"})
+
+
+@bp.route("/favourites", methods=["POST"])
+def add_movie_to_favourites():
+    return jsonify({"message": "Adding movie to favourite success"})
+
+
+@bp.route("/search", methods=["GET"])
+def search():
+    return jsonify({"message": "Searching success"})
+
+
+@bp.route("/movies/detail/<id>", methods=["GET"])
+def get_movie_by_id(id):
+    return jsonify({"movie_id": id})
+
+
+@bp.route("/movies/<genre>", methods=["GET"])
+def get_movies_by_genre(genre):
+    return jsonify({"genre": genre})
+
+
+@bp.route("/upcoming", methods=["GET"])
+def get_upcoming_movies():
+    return jsonify({"message": "Getting upcoming success"})
+
+
+@bp.route("/trending", methods=["GET"])
+def get_trending_movies():        
+    return jsonify({"message": "Getting trending success"})
+
+
+@bp.route("/trailers", methods=["GET"])
+def get_trailers():
+    return jsonify({"message": "Getting trailers success"})
