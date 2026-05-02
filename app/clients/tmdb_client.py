6fa9d99709d440e1df682834990a90c663569f03
zadnap <88957683+zadnap@users.noreply.github.com>
2026-05-02 14:50:58 +0700

    fix(tmdb): increase request timeout from 5 to 10

--- app/clients/tmdb_client.py
+++ app/clients/tmdb_client.py
@@ -19,7 +19,7 @@ class TMDBClient:
 
         url = f"{TMDBClient.BASE_URL}{endpoint}"
 
-        response = requests.get(url, params=params, timeout=5)
+        response = requests.get(url, params=params, timeout=10)
         response.raise_for_status()
 
         return response.json()
